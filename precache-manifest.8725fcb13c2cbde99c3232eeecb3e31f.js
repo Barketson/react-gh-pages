ff2e0320f61d4fff33a788a1551b580e3db46027
Barketson <50872144+Barketson@users.noreply.github.com>
2019-09-15 17:46:59 +0300

    Updates

---
+++ precache-manifest.8725fcb13c2cbde99c3232eeecb3e31f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "429c1e6b2b1e1e9f58be8d13bf772b63",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "0941310680dec62b9ad8",
+    "url": "/react-gh-pages/static/css/2.d4c5f937.chunk.css"
+  },
+  {
+    "revision": "73e1fd21809b3b1e833b",
+    "url": "/react-gh-pages/static/css/main.ff53bda9.chunk.css"
+  },
+  {
+    "revision": "0941310680dec62b9ad8",
+    "url": "/react-gh-pages/static/js/2.e59a54b9.chunk.js"
+  },
+  {
+    "revision": "73e1fd21809b3b1e833b",
+    "url": "/react-gh-pages/static/js/main.85f6c4f9.chunk.js"
+  },
+  {
+    "revision": "6e67beebd50093544e61",
+    "url": "/react-gh-pages/static/js/runtime~main.b8ff0978.js"
+  },
+  {
+    "revision": "cf08dfb1a6b4bc580f773dd8f188debd",
+    "url": "/react-gh-pages/static/media/computer.cf08dfb1.png"
+  },
+  {
+    "revision": "705106e2d19f5de6269bcc42f35113f7",
+    "url": "/react-gh-pages/static/media/foto.705106e2.png"
+  },
+  {
+    "revision": "ea2b941fc784d12ce0ae665811ccaf12",
+    "url": "/react-gh-pages/static/media/portfolio.ea2b941f.png"
+  },
+  {
+    "revision": "3ac6700038085a1180c0d7757c93dff3",
+    "url": "/react-gh-pages/static/media/weather.3ac67000.png"
+  }
+]);
